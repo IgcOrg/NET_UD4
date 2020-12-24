4665084769a8742acf740d502600d54ba2ea055c
Iago <iago.gc88@gmail.com>
2020-12-24 12:52:48 +0100

    E14

---
+++ U4_E14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_E14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radio;
+
+            Console.WriteLine("Introduce el radio del círculo: ");
+            radio = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("La superfície de tu círculo mide {0}", Math.Round(Math.PI * Math.Pow(radio, 2), 2));
+
+            Console.ReadKey();
+        }
+    }
+}
