d161d9e1e0ee165c8ef410b2e0314f4b669b3880
Iago <iago.gc88@gmail.com>
2020-12-24 12:59:03 +0100

    E15

---
+++ U4_E15/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_E15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca un número: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num % 2 == 0) {
+                Console.WriteLine("El número es divisible por dos.");
+            } else {
+                Console.WriteLine("El número no es divisible por dos.");
+            }                
+
+            Console.ReadKey();
+        }
+    }
+}
