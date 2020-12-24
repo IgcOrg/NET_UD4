d8ae034fcebbb6a5173ca7c72caa80d7be7cd7c9
Iago <iago.gc88@gmail.com>
2020-12-24 12:47:09 +0100

    E13

---
+++ U4_E13/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_E13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.WriteLine("Introduce tu nombre");
+            nombre = Console.ReadLine();
+
+
+            Console.Write("Bienvenido {0}.", nombre);
+
+            Console.ReadKey();
+        }
+    }
+}
