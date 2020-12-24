37fd398314e5097b4ecbffd9f21443a93018d7bc
Iago <iago.gc88@gmail.com>
2020-12-24 12:43:04 +0100

    E12

---
+++ U4_E12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_E12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Iago";
+
+            Console.Write("Bienvenido {0}.", nombre);
+
+            Console.ReadKey();
+        }
+    }
+}
