46bab30ce0b3d36c3737afab958c00d0da291cf1
Iago <iago.gc88@gmail.com>
2020-12-24 11:02:54 +0100

    E1

---
+++ U4_E1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U4_E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            a = 4;
+            b = 2;
+            c = 5;
+
+            Console.WriteLine("Suma={0}", a + b + c);
+            Console.ReadKey();
+        }
+    }
+}
